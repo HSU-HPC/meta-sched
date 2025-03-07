41a935713d29cd7cf80874cc036514ccd9b7fc77
Ruben Horn <>
2025-03-07 12:39:40 +0100

    #cores weighted stochastic scheduling

--- meta_sched/scheduler/stochastic.py
+++ meta_sched/scheduler/stochastic.py
@@ -1,5 +1,5 @@
 import random
-from typing import List
+from typing import List, Self
 
 from meta_sched.common.job import Spec
 from meta_sched.common.scheduling_decision import (
@@ -7,6 +7,7 @@ from meta_sched.common.scheduling_decision import (
     Impossible,
     SchedulingDecision,
 )
+from meta_sched.common.target import Target
 from meta_sched.scheduler import Scheduler
 
 
@@ -19,3 +20,18 @@ class Uniform(Scheduler):
             if target_id in self._targets:
                 return Assigned(wait_seconds=0, target_id=target_id)
         return Impossible()
+    
+class WeightedCores(Scheduler):
+    def __init__(self: Self, targets: List[Target]) -> None:
+        super().__init__(targets)
+        self.__weights = {t.id: t.__nodes * t.__cores_per_node for t in targets}
+
+    def request_schedule(
+        self, job_spec: Spec, available_targets: List[str]
+    ) -> SchedulingDecision:
+        available_targets = [t for t in available_targets if t in self._targets]
+        if len(available_targets) == 0:
+            return Impossible()
+        weights = [self.__weights[t] for t in available_targets]
+        selected = random.choices(available_targets, weights, k=1)[0]
+        return Assigned(wait_seconds=0, target_id=selected)
