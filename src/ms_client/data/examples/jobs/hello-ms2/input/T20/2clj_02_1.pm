ae570bff5a3f44a72b4307111cda3f3bcd4ad2f3
Ruben Horn <>
2026-02-26 15:52:54 +0100

    Added ms2 example

---
+++ src/ms_client/data/examples/jobs/hello-ms2/input/T20/2clj_02_1.pm
@@ -0,0 +1,35 @@
+NSiteTypes = 2
+
+SiteType   =  LJ126
+NSites   =  2
+
+x = 0
+y = 0
+z = -0.1
+sigma = 1.0
+epsilon = 1.0
+mass = 1.0
+
+x = 0
+y = 0
+z = 0.1
+sigma = 1.0
+epsilon = 1.0
+mass = 1.0
+
+
+SiteType   =  Quadrupole
+NSites     =   1
+
+x          =   0.0
+y          =   0.0
+z          =   0.0
+theta      =   0.0
+phi        =   0.0
+quadrupole   =   0.01175014
+mass       =   0.0
+shielding  =   0.5
+
+NRotAxes   =   2
+InertMomX  =  0.02
+InertMomY  =  0.02
---
+++ src/ms_client/data/examples/jobs/hello-ms2/input/T20/README.md
@@ -0,0 +1,5 @@
+# Example Configuration files for ms2
+
+Provided by Denis Saric (TU Berlin).
+
+**NOTE:** The ms2 source code must be copied to `./input/ms2_src`.
